ecdb0c57250c5e19f8c5c153955162d8de98d3a4
Débora Rodrigues <paulohenrique8190@gmail.com>
2021-09-02 17:15:46 -0300

    Estilização e construção da tela de Converter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Conversor from './components/Conversor/Conversor';
 
 function App() {
   return (
     <div className="App">
-      
+      <Conversor moedaA="EUR" moedaB="BRL"/>
     </div>
   );
 }
--- src/components/Conversor/Conversor.js
+++ src/components/Conversor/Conversor.js
@@ -1,9 +1,50 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+export default function Conversor(props) {
+
+    const [moedaA, setMoedaA] = useState("");
+    const [moedaB, setMoedaB] = useState(0);
 
 
-export default function Conversor() {
+    async function converter() {
+        console.log(moedaA);
+
+    }
+
     return (
-        <>
+        <React.Fragment>
+            <div className="Conversor">
+                <h2>Converter moeda {props.moedaA} para {props.moedaB}</h2>
+                <div>
+                    <input
+                        type="text"
+                        placeholder="digite o valor"
+                        onChange={(e) => { setMoedaA(e.target.value) }}
+                    />
+                    <select>
+                        <option value="Currency">Currency</option>
+                    </select>
+                </div>
+                <span>=</span>
+                <div>
+                    <input
+                        type="text"
+                        placeholder="digite o valor"
 
-        </>
+                    />
+                    <select>
+                        <option value="Currency">Currency</option>
+                    </select>
+                </div>
+                <button
+                    type="button"
+                    onClick={converter}
+                >
+                    Converter
+                </button>
+                <h2>Valor convertido</h2>
+            </div>
+        </React.Fragment>
     );
 }
