8060f820c240258c56ef4c6640e27a0fcc98fa51
Débora Rodrigues <paulohenrique8190@gmail.com>
2021-09-13 09:51:52 -0300

    Carregamento da api para buscar á cotação das moedas atualmente

--- src/components/Conversor/Conversor.js
+++ src/components/Conversor/Conversor.js
@@ -1,20 +1,37 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
+
 import "./styles.css";
 
 export default function Conversor(props) {
 
     const [moedaA, setMoedaA] = useState("");
     const [moedaB, setMoedaB] = useState(0);
-    const [select, setSelect] = useState([]);
+    const [currency, setCurrency] = useState([]);
+    const [select1, setSelect1] = useState("");
+    const [select2, setSelect2] = useState("");
     const [input1, setInput1] = useState("");
     const [input2, setInput2] = useState("");
 
- 
 
-    useEffect(async () => {
-        setSelect(["dados"]);
+
+    useEffect(() => {
+        async function data(){
+            const response = await axios.get(process.env.REACT_APP_CURRENCY_API_URL);
+            console.log(response);
+        }
+        
+        data();
 
     }, []);
+
+
+    function teste() {
+        console.log(select1);
+        console.log(select2);
+        console.log(input1);
+        console.log(input2);    
+    }
 
     return (
         <React.Fragment>
@@ -27,24 +44,32 @@ export default function Conversor(props) {
                         value={input1}
                         onChange={(e) => { setInput2(e.target.value) }}
                     />
-                    {select.map(item => {
-                        <select>
+                    <select onChange={(e) => { setSelect1(e.target.value) }}>
+                        {currency.map(item => {
+
                             <option value={item}>{item}</option>
-                        </select>
-                    })}
+
+                        })}
+                    </select>
                 </div>
                 <span>=</span>
                 <div>
                     <input
                         type="text"
                         placeholder="digite o valor"
-                        
+                        value={input2}
+                        onChange={(e) => { setInput1(e.target.value) }}
                     />
-                    <select>
-                        <option value="Currency">Currency</option>
+                    <select onChange={(e) => { setSelect2(e.target.value) }}>
+                        {currency.map(item => {
+
+                            <option value={item}>{item}</option>
+
+                        })}
                     </select>
                 </div>
             </div>
+            <button type="button" onClick={teste}></button>
         </React.Fragment>
     );
 }
